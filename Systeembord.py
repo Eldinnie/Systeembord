1e1f21d6833cf3778d5d59a4987b6ec8c022d576
Eldinnie <Pieter.schutz+github@gmail.com>
2015-09-18 00:00:02 +0200

    last version

--- Systeembord.py
+++ Systeembord.py
@@ -1,19 +1,39 @@
-__author__ = 'Pieter'
-'''
+#!/usr/bin/env python
+"""
 Created on 19 jan. 2014
 
-@author: Pieter
-'''
+@author: Pieter Schutz
+Licensed with GPL.
+
+"""
 
 import sys
-import objects
 from pygame.locals import *
-from objects.Objects import ClassicBoard, NewBoard, TwoCounters
+import objects
+from objects.Objects import *
 from objects import *
+import Tkinter as tk
+from tkFileDialog import askopenfilename, asksaveasfilename
 
+__author__ = "Pieter Schutz"
+__copyright__ = "Copyright 2015, Pieter Schutz"
+__credits__ = ["Pieter Schutz"]
+__license__ = "GPL"
+__version__ = "0.3.0"
+__maintainer__ = "Pieter Schutz"
+__email__ = "schutz@ggh.nu"
+__status__ = "Development"
 
-def buildbord(boardclass):
+def buildbord(boardclass, connections=None, values=None):
     bord = boardclass()
+    if connections:
+        bord.connections = connections
+    if values:
+        for i, it in values.items():
+            bord.itemdict[i].value = it
+            bord.itemdict[i].refresh()
+            bord.itemdict[i].par.refresh()
+    bord.loop_connections()
     return bord, bord.ins, bord.outs, bord.buttons
 
 
@@ -78,7 +98,7 @@ def main():
                             sliding = True
                             last_position = cur
 
-                            # check left mouse button up
+            # check left mouse button up
         for event in pygame.event.get(MOUSEBUTTONUP):
             if event.type == MOUSEBUTTONUP and event.button == 1:
                 current = event.pos
@@ -98,7 +118,7 @@ def main():
                             # if so, reset the temp start point
                             start = None
                             current = None
-                            tmp_connection = (startblokje, i)
+                            tmp_connection = (startblokje.id, i.id)
                             # check if the connection already exists
                             for con in bord.connections:
                                 if tmp_connection == con:
@@ -108,7 +128,7 @@ def main():
                                     break
                             else:
                                 # add to the connection list and notify a special button
-                                bord.connections.append((startblokje, i))
+                                bord.connections.append((startblokje.id,i.id))
                                 if i.__class__ == objects.Objects.OnOffButtonValue:
                                     i.notify_connect()
                                 change = True
@@ -133,7 +153,7 @@ def main():
                     if button[0].collidepoint(cur):
                         pygame.event.post(button[1])
 
-                        # release of right mouse button
+                # release of right mouse button
             if event.type == MOUSEBUTTONUP and event.button == 3:
                 cur = event.pos
                 for out in outs:
@@ -141,10 +161,10 @@ def main():
                     if out.surf.get_rect().collidepoint(cur[0] - offset_x, cur[1] - offset_y):
                         # print mouse on an output, removing last made link from this output"
                         for i in range(len(bord.connections) - 1, -1, -1):
-                            if bord.connections[i][0] == out:
-                                bord.connections[i][1].set(LOW)
-                                if bord.connections[i][1].__class__ == objects.Objects.OnOffButtonValue:
-                                    bord.connections[i][1].notify_disconnect()
+                            if bord.connections[i][0] == out.id:
+                                bord.itemdict[bord.connections[i][1]].set(LOW)
+                                if bord.itemdict[bord.connections[i][1]].__class__ == objects.Objects.OnOffButtonValue:
+                                    bord.itemdict[bord.connections[i][1]].notify_disconnect()
                                 bord.connections.pop(i)
                                 change = True
                                 break
@@ -159,7 +179,10 @@ def main():
                             change = True
                             break
 
-        # dragging mouse while started from an output
+
+
+
+            # dragging mouse while started from an output
         for event in pygame.event.get(MOUSEMOTION):
             if event.type == MOUSEMOTION and isdown and start:
                 current = event.pos
@@ -173,26 +196,27 @@ def main():
                 last_position = cur
                 change = True
 
-        # checking if the clock ticked
+
+            # checking if the clock ticked
         for i in range(1, 5, 1):
-            if pygame.event.peek(USEREVENT + i):
-                event = pygame.event.get(USEREVENT + i)
-                for out in [x for x in outs if x.__class__ == objects.Objects.ValueField and x.id]:
-                    if out.id == i:
+            if pygame.event.peek(USEREVENT+i):
+                event = pygame.event.get(USEREVENT+i)
+                for out in [x for x in outs if x.__class__ == objects.Objects.ValueField and x.countid]:
+                    if out.countid == i:
                         out.set(HIGH)
                         change = True
                         last_loop_ticked_pulses.append(out)
-
+        
         for event in pygame.event.get(USEREVENT):
             if event.action == RESET:
-                bord.connections = []
-                for i in [x for x in ins if x.__class__ == objects.Objects.ValueField]:
-                    i.set(HIGH)
-                    i.set(LOW)
-                for i in [x for x in ins if x.__class__ == objects.Objects.Slider]:
-                    i.set(2.5)
-                for out in [x for x in outs if x.__class__ == objects.Objects.ValueField and x.id]:
-                    out.par.inA.set(1)
+                    bord.connections = []
+                    for i in [x for x in ins if x.__class__ == objects.Objects.ValueField]:
+                        i.set(HIGH)
+                        i.set(LOW)
+                    for i in [x for x in ins if x.__class__ == objects.Objects.Slider]:
+                        i.set(2.5)
+                    for out in [x for x in outs if x.__class__ == objects.Objects.ValueField and x.countid]:
+                        out.par.inA.set(1)
 
             if event.action == BOARD:
                 if type(bord) == event.code:
@@ -200,6 +224,30 @@ def main():
                 else:
                     bord, ins, outs, buttons = buildbord(event.code)
                     pygame.event.post(pygame.event.Event(pygame.USEREVENT, action=RESET))
+            if event.action == SAVE:
+                options = {}
+                options['defaultextension'] = ".sbs"
+                options['filetypes'] = [('systeembord files', '.sbs'), ('all files', '.*')]
+                options['title'] = 'Opslaan'
+                options['initialfile'] = bord.__class__.__name__+'.sbs'
+                root = tk.Tk()
+                root.withdraw()
+                file_path = asksaveasfilename(**options)
+                if file_path:
+                    with open(file_path, "w") as f:
+                        f.write(repr(bord))
+            if event.action == LOAD:
+                options = {}
+                options['defaultextension'] = ".sbs"
+                options['filetypes'] = [('systeembord files', '.sbs'), ('all files', '.*')]
+                root = tk.Tk()
+                root.withdraw()
+                file_path = askopenfilename(**options)
+                if file_path:
+                    with open(file_path, "r") as f:
+                        loaded = eval(f.read())
+                    bord, ins, outs, buttons = buildbord(loaded['board'], connections=loaded['connections'], values=loaded['values'])
+
 
         if change:
             bord.loop_connections()
@@ -208,15 +256,12 @@ def main():
         display_surface.fill(BACKGROUND)
         display_surface.blit(bord.surf, bord.surf.get_rect())
         fps = fps_clock.get_fps()
-        surf, rect = draw_text("FPS: " + str(fps), font_small, RED)
-        rect.bottomright = (display_surface.get_rect().bottomright)
-        display_surface.blit(surf, rect)
         # drawing connection
         for o, i in bord.connections:
-            offset_x_out, offset_y_out = o.surf.get_abs_offset()
-            offset_x_in, offset_y_in = i.surf.get_abs_offset()
-            xo, yo = o.surf.get_rect().center
-            xi, yi = i.surf.get_rect().center
+            offset_x_out, offset_y_out = bord.itemdict[o].surf.get_abs_offset()
+            offset_x_in, offset_y_in = bord.itemdict[i].surf.get_abs_offset()
+            xo, yo = bord.itemdict[o].surf.get_rect().center
+            xi, yi = bord.itemdict[i].surf.get_rect().center
             pygame.draw.line(display_surface, RED, (xo + offset_x_out, yo + offset_y_out),
                              (xi + offset_x_in, yi + offset_y_in), 3)
 
