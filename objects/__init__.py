179dbb864a776c764e9dfc16f8a3cbb6189228a6
Pieter Schutz <pieter.schutz@gmail.com>
2016-03-07 18:17:17 +0100

    Updates for packaging

--- objects/__init__.py
+++ objects/__init__.py
@@ -1,13 +1,19 @@
 import pygame
 import os
 
+import sys
+def resource_path(relative):
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relative)
+    return os.path.join(relative)
+
 pygame.init()
 pygame.mixer.init()
 HIGH = True
 LOW = False
-font_small = pygame.font.Font(os.path.join("Items", "freesansbold.ttf"), 11)
-font_medium = pygame.font.Font(os.path.join("Items", "freesansbold.ttf"), 20)
-font_big = pygame.font.Font(os.path.join("Items", "freesansbold.ttf"), 32)
+font_small = pygame.font.Font(resource_path(os.path.join("Items", "freesansbold.ttf")), 11)
+font_medium = pygame.font.Font(resource_path(os.path.join("Items", "freesansbold.ttf")), 11)
+font_big = pygame.font.Font(resource_path(os.path.join("Items", "freesansbold.ttf")), 11)
 
 fps_clock = pygame.time.Clock()
 RED = (255, 0, 0)
@@ -20,13 +26,16 @@ RESET = 9
 BOARD = 8
 SAVE = 10
 LOAD = 11
-beep = pygame.mixer.Sound(os.path.join("Items", "Beep.wav"))
+beep = pygame.mixer.Sound(resource_path(os.path.join("Items", "Beep.wav")))
 
 
-def draw_text(text, font, color,back=BACKGROUND):
-    text_surface = font.render(text, True, color,back)
+
+
+def draw_text(text, font, color, back=BACKGROUND):
+    text_surface = font.render(text, True, color, back)
     text_rect = text_surface.get_rect()
     return text_surface, text_rect
+
 
 def gen(n):
     num = n
